4cf5a1eae30f908ac88650cc4a7001cbb69c7ceb
Anubhav Singh <anubhavsinghgtm@gmail.com>
2021-01-05 23:58:00 +0530

    13th day

---
+++ Day13/insertionSort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+void insertionSort(int *arr, int n){
+int key;
+for(int i=0; i<n; ++i){
+    key = arr[i];
+    int j = i-1;
+    while(j >= 0 && arr[j]>key){
+        arr[j+1] =arr[j];
+        j = j-1;
+    }
+    arr[j+1] = key;
+}
+}
+
+int main(){
+int n;
+cin>>n;
+int arr[n];
+for(int i=0; i<n; ++i){
+    cin>>arr[i];
+}
+
+insertionSort(arr,n);
+
+for(int i=0; i<n; ++i){
+    cout<<arr[i];
+}
+return 0;
+}
